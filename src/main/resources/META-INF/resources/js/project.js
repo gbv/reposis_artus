80d142144259d146ff23d779c6ffad44c9b0e7f4
Antoniafriedrich <antonia.friedrich@gbv.de>
2025-10-23 16:52:16 +0200

    ARTUS-48-Pflichtfeld-von-Dissertationen

--- src/main/resources/META-INF/resources/js/project.js
+++ src/main/resources/META-INF/resources/js/project.js
@@ -17,4 +17,25 @@ $(document).ready(function() {
     return true;
   });
 
+  //Disserations have a xed:validator for related item. Though disabled fields are ignored during validation, making it seem as if the field does not exist.
+  //Therefore the disabled title field must be automatically enabled before the document is sent.
+    document.querySelectorAll('button[name="_xed_submit_servlet:CreateObjectServlet"], button[name="_xed_submit_servlet:UpdateObjectServlet"]')
+        .forEach(btn => {
+            const form = btn.closest('form');
+            function onClick(e) {
+                e.preventDefault();
+                form.querySelectorAll('.mir-fieldset-content.mir-related-item-search input[disabled]')
+                    .forEach(i => {
+                        i.removeAttribute('disabled');
+                    });
+                btn.removeEventListener('click', onClick);
+                setTimeout(() => {
+                    btn.click();
+                }, 0);
+            }
+            btn.addEventListener('click', onClick);
+        });
+
+
+
 });
